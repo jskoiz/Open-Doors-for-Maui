696f63e7efe4397ff511dcbaf5e2f0ca4ecc9c27
jskoiz <jskoizumi@gmail.com>
2023-08-14 13:25:45 -1000

    Resolved merge conflicts and merged main into development

--- next.config.js
+++ next.config.js
@@ -1,27 +1,5 @@
 module.exports = {
   async redirects() {
-    return [
-      {
-        source: "/join",
-        destination: "/join/01-you",
-        permanent: true,
-      },
-      {
-        source: "/nominate",
-        destination: "/join/01-you",
-        permanent: true,
-      },
-      {
-        source: "/discord",
-        destination: "https://discord.gg/WHpCrPqeqx",
-        permanent: false,
-      },
-      {
-        source: "/linkedin",
-        destination:
-          "https://www.linkedin.com/company/hawaiians-in-technology/",
-        permanent: false,
-      },
-    ];
+    return [];
   },
 };
